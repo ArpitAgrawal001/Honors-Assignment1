a3bc7a9eaa446fa6697794ae3130e78c745cd689
ArpitAgrawal001 <agrawa271@gmail.com>
2024-02-16 15:07:17 +0530

    Requirement3 Completed

--- Requirement2/Poem.js
+++ Requirement2/Poem.js
@@ -1,22 +1,21 @@
-// Imported the 'readFileSync' function the 'fs' (file system) module
+// Imported the 'readFileSync' function from the 'fs' module
 import { readFileSync } from 'fs';
 
 // Function to print the content of poem.txt synchronously
 function printPoemContent() {
     try {
-        // Read the file 'poem.txt' and store its content in the 'data' variable
-        // The file is read in 'utf8' encoding
+        // Reading the file 'poem.txt' synchronously and store its content in the 'data' variable
         const data = readFileSync('poem.txt', 'utf8');
 
-        // Log the content of 'data' to the console
+        // Printing the content of 'data' to the console
         console.log(data);
     } 
-    // If there's an error while reading the file, catch it here
+    // Catching errors that might occur during file reading
     catch (err) {
-        // Log an error message to the console, including the error details
+        // Printing an error message to the console if an error occurs
         console.error('Error reading the file:', err);
     }
 }
 
-// Call the 'printPoemContent' function to print the poem content
+// Calling the function to print the poem content
 printPoemContent();
